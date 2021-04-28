bc65f25ccf3e7a0ad9643a8f5d2aa0c6de5adaac
BIT <BIT@DESKTOP-OTC3N9B>
2021-04-28 17:12:47 +0900

    TV[실습6]

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -5,26 +5,54 @@ public class TV {
 	private int volume;		// 0~100
 	private boolean power;	
 	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+	
 	// 게터세터말고 함수 만들어서
 	public void power(boolean on) {
-		
+		power = on;
 	}
 	
 	public void channel(boolean up) {
-		
+		channel(channel + (up ? 1 : -1));
 	}
 	
 	public void channel(int channel) {
-
-	}
-	public void volume(boolean up) {
+		if(!power) {
+			return;
+		}
 		
+		if(channel < 1) {
+			channel = 255;
+		} else if(channel > 255) {
+			channel = 1;
+		}
+
+		this.channel = channel;
 	}
 	
-	public void volume(int channel) {
-
+	public void volume(boolean up) {
+		volume(volume + (up ? 1 : -1));
 	}
+	
+	public void volume(int volume) {
+		if(!power) {
+			return;
+		}
+		
+		if(volume < 0) {
+			volume = 100;
+		} else if(volume > 100) {
+			volume = 0;
+		}
+		
+		this.volume = volume;
+	}
+	
 	public void status() {
-		System.out.println("TV[power=on, channel=11, volume=30]");
+		System.out.println("TV[power=" + (power ? "on" : "off") + ", " + "channel=" + channel + ", " + "volume=" + volume + "]");
 	}
 }
