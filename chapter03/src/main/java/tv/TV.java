ffed195ffa0e2bbc7388f4a876bdae5239ad1270
BIT <BIT@DESKTOP-OTC3N9B>
2021-04-28 15:38:39 +0900

    예제[실습6]

---
+++ chapter03/src/main/java/tv/TV.java
@@ -0,0 +1,30 @@
+package tv;
+
+public class TV {
+	private int channel;	// 1~255
+	private int volume;		// 0~100
+	private boolean power;	
+	
+	// 게터세터말고 함수 만들어서
+	public void power(boolean on) {
+		
+	}
+	
+	public void channel(boolean up) {
+		
+	}
+	
+	public void channel(int channel) {
+
+	}
+	public void volume(boolean up) {
+		
+	}
+	
+	public void volume(int channel) {
+
+	}
+	public void status() {
+		System.out.println("TV[power=on, channel=11, volume=30]");
+	}
+}
