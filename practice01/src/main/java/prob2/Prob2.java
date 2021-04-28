a706c5a6bbd2bd72c8f6501dd0689d16560313ac
BIT <BIT@DESKTOP-OTC3N9B>
2021-04-28 09:06:59 +0900

    practice

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -3,7 +3,11 @@ package prob2;
 public class Prob2 {
 	public static void main(String[] args) {
 		/* 코드 작성 */
-		int a = 10;
-		int b = 20;
+		for (int i = 1; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print(i + j + " ");
+			}
+			System.out.println();
+		}
 	}
 }
--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -7,12 +7,27 @@ public class Prob3 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-
-		/* 코드 작성 */
-		int a = scanner.nextInt();
-		double b = scanner.nextDouble();
-		String c = scanner.nextLine();
-		
+		System.out.print("숫자를 입력하세요: ");
+	      int num = scanner.nextInt();
+	      int sum = 0;   
+	      
+	      if(num%2 != 0) { 
+	         for (int i = 0; i <= num; i++) {
+	            if(i%2 != 0) {
+	               sum += i;
+	            }
+	         }
+	         System.out.print("결과 값 : " + sum);
+	      } else {
+	         for (int i = 0; i <= num; i++) {
+	            if(i%2 == 0) {
+	               sum += i;
+	            }
+	         }         
+	         System.out.print("결과 값 : " + sum);
+	      }
+	
 		scanner.close();
 	}
 }
+
