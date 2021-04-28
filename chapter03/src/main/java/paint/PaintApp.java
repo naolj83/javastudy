8894a333e8d5c8bb85c6f0d93ed1a79f8f2d724e
BIT <BIT@DESKTOP-OTC3N9B>
2021-04-28 16:03:00 +0900

    객체지향프로그래밍-다형성(오버라이딩#1)

--- chapter03/src/main/java/paint/PaintApp.java
+++ chapter03/src/main/java/paint/PaintApp.java
@@ -8,12 +8,17 @@ public class PaintApp {
 		point1.setX(10);
 		point1.setY(20);
 		point1.show();
-
+		point1.show(false);
+		
 		Point point2 = new Point(100, 200);
 		point2.show();
 		
-		point1.show(false);
-		
+		Point point3 = new ColorPoint();
+		point3.setX(50);
+		point3.setY(100);
+		((ColorPoint)point3).setColorl("red");	//다운캐스팅
+		// point3.show();
+		point3.show(true);
 	}
 	
 
