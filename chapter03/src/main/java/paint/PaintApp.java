ad3957a114ade6c42b5fa66a7e1e4cfd7d61704a
BIT <BIT@DESKTOP-OTC3N9B>
2021-04-29 11:30:50 +0900

    객체지향프로그래밍(추상클래스+인터페이스)

--- chapter03/src/main/java/paint/PaintApp.java
+++ chapter03/src/main/java/paint/PaintApp.java
@@ -6,7 +6,7 @@ public class PaintApp {
 		Point point1 = new Point(10, 20);
 		point1.show();
 		point1.show(false);
-		
+
 		Point point2 = new Point(100, 200);
 		// point2.show();
 		// drawPoint(point2);
@@ -16,20 +16,45 @@ public class PaintApp {
 		// point3.show();
 		// point3.show(true);
 		draw(point3);
-		
+
 		Rect rect = new Rect();
 		// drawRect(rect);
 		// drawShape(rect);
 		draw(rect);
-		
+
 		Triangle triangle = new Triangle();
 		// drawTriangle(triangle);
 		// drawShape(triangle);
 		draw(triangle);
-		
+
 		Circle circle = new Circle();
 		// drawShape(circle);
 		draw(circle);
+
+		// Drawable drawable = new GraphicText("hello~");
+		// =
+		draw(new GraphicText("hello~"));
+
+		//
+		// instanceof test
+		//
+		System.out.println(circle instanceof Object);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Circle);
+		// 오류: class는 hierachy 상위와 하위만
+		//		instanceof 연산자를 사용할 수 있다.
+		// System.out.println(circle instanceof Rect);
+		
+		Shape s = new Circle();
+		System.out.println(s instanceof Object);
+		System.out.println(s instanceof Shape);
+		System.out.println(s instanceof Circle);		
+		System.out.println(s instanceof Rect);		
+		
+		// interface는 hierachy와 상관없이 
+		// instanceof 연산자를 사용할 수 있다.
+		System.out.println(s instanceof Drawable);
+		System.out.println(s instanceof Runnable);
 	}
 
 	public static void draw(Drawable drawable) {
@@ -55,5 +80,5 @@ public class PaintApp {
 //	public static void drawTriangle(Triangle triangle) {
 //		triangle.draw();
 //	}
-	
+
 }
