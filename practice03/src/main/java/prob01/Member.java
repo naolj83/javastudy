f08d03dc2931757119f98d4630f6204d066a5c65
BIT <BIT@DESKTOP-OTC3N9B>
2021-04-30 17:36:05 +0900

    practice

--- practice03/src/main/java/prob01/Member.java
+++ practice03/src/main/java/prob01/Member.java
@@ -1,11 +1,11 @@
 package prob01;
 
 public class Member {
-	
+
 	private String id;
 	private String name;
 	private int point;
-	
+
 	public Member(String id, String name, int point) {
 		this.id = id;
 		this.name = name;
@@ -15,25 +15,29 @@ public class Member {
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getPoint() {
 		return point;
 	}
+
 	public void setPoint(int point) {
 		this.point = point;
 	}
-	
+
 	public void show() {
 		System.out.println("id: " + id + ", " + "name: " + name + ", " + "point: " + point + "점");
 	}
-	
-	
+
 }
