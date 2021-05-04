83fb2ec8d923e82db0f6f5513c2fc06cfaee5a5b
BIT <BIT@DESKTOP-OTC3N9B>
2021-05-04 13:32:57 +0900

    TCP C/S 와꾸 코드 작성 연습

--- chapter04/src/main/java/network/test/TCPServer.java
+++ chapter04/src/main/java/network/test/TCPServer.java
@@ -36,6 +36,7 @@ public class TCPServer {
 				OutputStream os = socket.getOutputStream();
 
 				while (true) {
+					// 4. 데이터 읽기
 					byte[] buffer = new byte[256];
 					int readByteCount = is.read(buffer); // blocking
 					if (readByteCount == -1) {
@@ -46,7 +47,12 @@ public class TCPServer {
 
 					String data = new String(buffer, 0, readByteCount, "utf-8");
 					System.out.println("[server] received:" + data);
+
+					// 5. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
 				}
+			} catch (SocketException e) {
+				System.out.println("[server] suddenly closed by client");
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
