cce9bfce657887bd0d5d4f36bd66817722871736
BIT <BIT@DESKTOP-OTC3N9B>
2021-05-07 10:29:02 +0900

    socket options & algorithm

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -11,77 +11,81 @@ import java.net.SocketException;
 public class TCPServer {
 
 	public static void main(String[] args) {
-		ServerSocket serverSocket = null; 
-				
+		ServerSocket serverSocket = null;
+
 		try {
 			// 1. 서버소켓 생성
 			serverSocket = new ServerSocket();
-			
+
+			// 1-1. Time-Wait 상태에서도 소켓에 포트 번호
+			// 할당이 가능하게 하기 위해서
+			serverSocket.setReuseAddress(true);
+
 			// 2. 바인딩(Binding)
-			//    Socket에 InetSocketAddress(IPAddress + Port)
-			//    IPAddress : 0.0.0.0 : 모든 IP 연결 허용
+			// Socket에 InetSocketAddress(IPAddress + Port)
+			// IPAddress : 0.0.0.0 : 모든 IP 연결 허용
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000));
-			
+
 			// 3. accept
-			//    클라이언트의 연결 요청을 기다린다.
+			// 클라이언트의 연결 요청을 기다린다.
 			Socket socket = serverSocket.accept(); // blocking
-			
-			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+
+			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
 			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
 			int remoteHostPort = inetRemoteSocketAddress.getPort();
-			System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remoteHostPort +"]");
-		
-			
-			
+			System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remoteHostPort + "]");
+
 			try {
 				// 4. IO Stream 받아오기
 				InputStream is = socket.getInputStream();
 				OutputStream os = socket.getOutputStream();
-				
-				while(true) {
+
+				while (true) {
 					// 5. 데이터 읽기
 					byte[] buffer = new byte[256];
 					int readByteCount = is.read(buffer); // blocking
-					if(readByteCount == -1) {
+					if (readByteCount == -1) {
 						// 클라이언트가 정상적으로 종료(close() 호출)
 						System.out.println("[server] closed by client");
 						break;
 					}
-					
+
 					String data = new String(buffer, 0, readByteCount, "utf-8");
 					System.out.println("[server] received:" + data);
-				
+
 					// 6. 데이터 쓰기
-					os.write(data.getBytes("utf-8"));
+					try {
+						Thread.sleep(2000);
+						os.write(data.getBytes("utf-8"));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
-			} catch(SocketException e) {
+			} catch (SocketException e) {
 				System.out.println("[server] suddenly closed by client");
-			} catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
 				try {
-					if(socket != null && socket.isClosed() == false) {
+					if (socket != null && socket.isClosed() == false) {
 						socket.close();
 					}
-				} catch(IOException e) {
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-			
-			
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(serverSocket != null && serverSocket.isClosed() == false) {
+				if (serverSocket != null && serverSocket.isClosed() == false) {
 					serverSocket.close();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
 
 	}
 
